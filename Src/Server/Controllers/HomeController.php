f53ef7a860f36653759bb75b596a9b2469a8161b
Neptune <vominhtri13@gmail.com>
2023-05-14 19:16:35 +0700

    .

--- Src/Server/Controllers/HomeController.php
+++ Src/Server/Controllers/HomeController.php
@@ -34,8 +34,47 @@ class HomeController extends Controller
         $this->view($this->signTemplate, []);
     }
 
-    function Timetable() {
-        $this->view($this->homeTemplate, []);
+    function Timetable($params) {
+        $model = $this->model("UserModel");
+
+        $queryHK = <<<HUH
+        SELECT DISTINCT HK, NAM FROM SinhVien_LHP WHERE MSSV='3121560004' ORDER BY NAM, HK DESC
+        HUH;
+        $hk = $model->getData($queryHK);
+
+        if (!isset($params[0]) || !isset($params[1])) {
+            $params[0] = $hk[0]["NAM"];
+            $params[1] = $hk[0]["HK"];
+        }
+
+        array_unshift($hk,[
+            "HK" => $params[1],
+            "NAM" => $params[0]
+        ]);
+
+        $hk = array_unique($hk, SORT_REGULAR);
+
+
+        $query = <<<WUT
+        SELECT SV.ID_DSMH, SL_SV, NGAY_BAT_DAU, TIET_BAT_DAU, TIET_KET_THUC, LOP, TUANHOC, TEN as TENMH, THU FROM SinhVien_LHP SV
+            JOIN Lop_Hoc_Phan LHP on LHP.ID_DSMH = SV.ID_DSMH and LHP.NAM_HOC = SV.NAM and LHP.HOC_KY = SV.HK
+            JOIN ThoiGianMonHoc TGMH on LHP.ID_DSMH = TGMH.ID_DSMH and LHP.NAM_HOC = TGMH.NAM and LHP.HOC_KY = TGMH.HK
+            LEFT JOIN MonHoc MH on MH.ID = LHP.ID_MONHOC
+        WHERE MSSV='3121560004' AND SV.NAM={$params[0]} AND SV.HK={$params[1]}
+        WUT;
+
+        $data = $model->getData($query);
+        foreach ($data as $index => $item) {
+            $DSMH = $item["ID_DSMH"];
+            $ID_NHOM = explode("_", $DSMH);
+            $data[$index]["MMH"]= $ID_NHOM[0];
+            $data[$index]["NHOM"]= $ID_NHOM[1];
+        }
+//        print_r($data);
+        $this->view($this->homeTemplate, [
+            "DSMH" => $data,
+            "HK" => $hk
+        ]);
     }
 
     function TimetableList($params) {
@@ -67,18 +106,12 @@ class HomeController extends Controller
         WHERE MSSV='3121560004' AND SV.NAM={$params[0]} AND SV.HK={$params[1]}
         WUT;
 
-
-
-//        print_r($hk);
-
-
-
         $data = $model->getData($query);
         foreach ($data as $index => $item) {
             $DSMH = $item["ID_DSMH"];
             $ID_NHOM = explode("_", $DSMH);
             $data[$index]["MMH"]= $ID_NHOM[0];
-            $data[$index]["NHOM"]= $ID_NHOM[0];
+            $data[$index]["NHOM"]= $ID_NHOM[1];
         }
 //        print_r($data);
         $this->view($this->homeTemplate, [
--- Src/Server/Views/Home/Timetable.php
+++ Src/Server/Views/Home/Timetable.php
@@ -1,37 +1,19 @@
 <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
 <!--<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>-->
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.7/index.global.min.js'></script>
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        let calendarEl = document.getElementById('calendar');
-        let calendar = new FullCalendar.Calendar(calendarEl, {
-            themeSystem: 'bootstrap5',
-            locale: "vi",
-            headerToolbar: {
-                start: 'title', // will normally be on the left. if RTL, will be on the right
-                center: 'dayGridMonth,timeGridWeek,listWeek',
-                end: 'today prev,next'
-            },
-            buttonText: {
-                today:    'Hôm nay',
-                month:    'Tháng',
-                week:     'Tuần',
-                day:      'Ngày',
-                list:     'Sự kiện'
-            },
-            titleFormat: {
-                year: 'numeric',
-                month: 'numeric',
-                day: 'numeric'
-            },
-            allDaySlot: false
-        });
-        calendar.render();
-    });
-</script>
+
 <!--<div class="d-flex justify-content-between">-->
 <!---->
 <!--</div>-->
+<script>
+    const optionHandler = (event) => {
+        location.href = `/Home/Timetable/${event.target.value}`
+    }
+</script>
+<?php
+//print_r($data["DSMH"]);
+
+?>
 
 <div class="mt-4 mb-4 d-flex justify-content-between">
     <h5>
@@ -41,15 +23,19 @@
         ?>
     </h5>
     <div class="btn-group" role="group" aria-label="Basic example">
-        <a href="TimetableList" type="button" class="btn btn-outline-secondary">Danh sách</a>
-        <a href="Timetable" type="button" class="btn btn-secondary">TKB</a>
+        <a href="/Home/TimetableList" type="button" class="btn btn-outline-secondary">Danh sách</a>
+        <a href="/Home/Timetable" type="button" class="btn btn-secondary">TKB</a>
     </div>
     <div>
-        <select class="form-select " aria-label="Default select example">
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+        <select class="form-select " onchange="optionHandler(event)">
+            <?php
+            foreach ($data["HK"] as $index => $datum) {
+                echo <<<HUH
+                    <option value="{$datum['NAM']}/{$datum["HK"]}">HK {$datum["HK"]} - Năm {$datum["NAM"]}</option>
+                HUH;
+
+            }
+            ?>
         </select>
     </div>
 </div>
@@ -57,4 +43,119 @@
 <div id='calendar'></div>
 
 
+<script>
+    <?php
+    $val = json_encode($data["DSMH"]);
+    echo "const data = JSON.parse('{$val}')"
+    ?>
 
+
+    let calendarEl = document.getElementById('calendar');
+    let calendar = new FullCalendar.Calendar(calendarEl, {
+        themeSystem: 'bootstrap5',
+        locale: "vi",
+        headerToolbar: {
+            start: 'title', // will normally be on the left. if RTL, will be on the right
+            center: 'dayGridMonth,timeGridWeek,listWeek',
+            end: 'today prev,next'
+        },
+        buttonText: {
+            today:    'Hôm nay',
+            month:    'Tháng',
+            week:     'Tuần',
+            day:      'Ngày',
+            list:     'Sự kiện'
+        },
+        titleFormat: {
+            year: '2-digit',
+            month: 'numeric',
+            day: 'numeric'
+        },
+        allDaySlot: false,
+        initialDate: `${data[0]["NGAY_BAT_DAU"]}`,
+        timeZone: 'UTC',
+    });
+
+    class FullCalendarTimetable {
+        constructor(data) {
+            this.data = data
+            this.timeTableStart = {
+                1 : "07:00:00",
+                2 : "07:50:00",
+                3 : "09:00:00",
+                4 : "09:50:00",
+                5 : "10:40:00",
+                6 : "13:00:00",
+                7 : "13:50:00",
+                8 : "15:00:00",
+                9 : "15:50:00",
+                10 : "16:40:00",
+                11 : "17:40:00",
+                12 : "18:40:00",
+                13 : "19:20:00",
+            }
+            this.timeTableEnd = {
+                1 : "07:50:00",
+                2 : "08:40:00",
+                3 : "09:50:00",
+                4 : "10:40:00",
+                5 : "11:30:00",
+                6 : "13:50:00",
+                7 : "14:40:00",
+                8 : "15:50:00",
+                9 : "16:40:00",
+                10 : "17:30:00",
+                11 : "18:30:00",
+                12 : "19:20:00",
+                13 : "20:10:00",
+            }
+            this.timeOffSet = {
+                "2" : 0,
+                "3" : 1,
+                "4": 2,
+                "5": 3,
+                "6": 4,
+                "7": 5,
+                "8": 6
+            }
+
+            this.startDate = data[0]["NGAY_BAT_DAU"]
+        }
+
+
+        toDate(initialDate, offset=0, week=0) {
+            const dateOffset = Number(week)*7 + Number(offset);
+            const date = new Date(initialDate);
+            date.setDate(date.getDate() + dateOffset)
+            return date
+        }
+
+        addToCalendar(){
+            for (const datum of this.data) {
+                console.log(datum["TUANHOC"].length)
+                for (let i = 0; i < datum["TUANHOC"].length; i++) {
+                    const value = datum["TUANHOC"][i]
+                    if (value === "-") {
+                        continue
+                    }
+                    const offset = this.timeOffSet[datum["THU"]]
+                    const offsetDate = this.toDate(this.startDate, offset, i)
+                    console.log(offsetDate)
+                    calendar.addEvent({
+                        title: `${datum["TENMH"]} - ${datum["LOP"]}`, // a property!
+                        start: `${offsetDate.toISOString().split("T")[0]}T${this.timeTableStart[datum["TIET_BAT_DAU"]]}Z`, // a property!
+                        end: `${offsetDate.toISOString().split("T")[0]}T${this.timeTableEnd[datum["TIET_KET_THUC"]]}Z`, // a property!
+                        // endTime: `${this.timeTableEnd[datum["TIET_KET_THUC"]]}`
+                    })
+
+                }
+            }
+        }
+
+
+    }
+    const timeTable = new FullCalendarTimetable(data);
+    timeTable.addToCalendar()
+
+    calendar.render();
+</script>
